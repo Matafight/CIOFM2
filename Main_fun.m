9e3a487974df6c5023f35c3f51aa53ec096806d3
Mata <guo_sc@foxmail.com>
2016-04-11 09:35:53 +0800

    delete redundant fun in main_fun.m

--- Main_fun.m
+++ Main_fun.m
@@ -29,72 +29,11 @@ result.acc=sum(totalacc)/20;
 result.accstd=std(totalacc);
 result.mae=sum(totalmae)/20;
 result.maestd=std(totalmae);
-
-rename=[dataname,'jieguo'];
+resultdir = './Result/'
+rename=[resultdir,dataname,'jieguo'];
 save(rename,'result','bestpars')
 
 
-
-
-
-function oneret=SetparaTrain(data,lambda,wtr,svdw,Ytr)
-
-
-[nrow,ncol]=size(data);
-%[B,ncol,wtr,wte,Ytr,Yte,dataname]=loadData(data);
-
-B=zeros(ncol,ncol);
-matD=B;
-matE=B;
-matF=B;
-matG=B;
-matH=B;
-g.gamma1=B;
-g.gamma2=B;
-g.gamma3=B;
-g.gamma4=B;
-g.gamma5=B;
-rho=1;
-
-oneret =ADMMmat(wtr,svdw,Ytr,lambda,B,matD,matE,matF,matG,matH,g,rho);
-
-
-
-function [svdw,wtr,wte,Ytr,Yte]=loadMyData(data,testdata);
-
-
-
-[datam,datan]=size(data);
-testrow=size(testdata,1);
-
-Xtr=data(:,1:datan-1);
-Ytr=data(:,datan);
-
-Xte=testdata(:,1:datan-1);
-Yte=testdata(:,datan);
-
-Xtr=[ones(datam,1),Xtr];
-Xte=[ones(testrow,1),Xte];
-
-B=zeros(datan+1,datan+1);   
-
-wtr=[];
-wte=[];
-for  i=1:datan
-    for j =1:datan
-   wtr=[wtr,Xtr(:,j).*Xtr(:,i)];
-   wte=[wte,Xte(:,j).*Xte(:,i)];
-    end
-end
-
-[u,s,v]=svd(wtr,'econ');
-grank=sum(sum(s~=0,2));
-s=s(1:grank,1:grank);
-u=u(:,1:grank);
-v=v(:,1:grank);
-svdw.s=s;
-svdw.u=u;
-svdw.v=v;
 
 
 
